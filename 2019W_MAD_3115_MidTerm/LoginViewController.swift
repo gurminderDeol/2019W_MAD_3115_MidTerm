843ec2d0303932d2d8daea4a109f17aaa9a68adb
MacStudent <macstudent@126-iMac11.local>
2019-03-20 18:57:47 -0400

    student class

--- 2019W_MAD_3115_MidTerm/LoginViewController.swift
+++ 2019W_MAD_3115_MidTerm/LoginViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var txtemail: UITextField!
+    
+    @IBOutlet weak var txtpassword: UITextField!
+    
+    @IBOutlet weak var swh: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func btnlogin(_ sender: UIButton) {
+        
+        
+        
+        if txtemail.text == "admin" && txtpassword.text == "admin@123"
+        {
+            let sb = UIStoryboard(name: "Main", bundle: nil)
+            let vc = sb.instantiateViewController(withIdentifier: "student") as! StudentEntryViewController
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+        else
+        {
+            let alert = UIAlertController(title: nil, message: "Invalid details", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(action)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
 }
 
--- 2019W_MAD_3115_MidTerm/Students/StudentEntryViewController.swift
+++ 2019W_MAD_3115_MidTerm/Students/StudentEntryViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class StudentEntryViewController: UIViewController {
 
+    @IBOutlet weak var txtstudentid: UITextField!
+    @IBOutlet weak var txtstudentName: UITextField!
+    @IBOutlet var seggender: UIView!
+    
+    @IBOutlet weak var txtemail: UITextField!
+    
+    @IBOutlet weak var txtm2: UITextField!
+    @IBOutlet weak var textm1: UITextField!
+    @IBOutlet weak var txtm3: UITextField!
+    @IBOutlet weak var txtm4: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
